f3ef61829a2467b9cfddc51ebcd7ff85b29e6224
Nikita Shah <niki.shah499@gmail.com>
2024-02-20 18:16:21 +0530

    structure change

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,4 +1,4 @@
 export { default as calculateTime } from'./calculateTime';
-export { default as healthCheck } from './statusCheck.ts';
+export { default as healthCheck } from './healthCheck';
 
 // export default { calculateTime, healthCheck };
