f3bf50af7afe45479d4e1d63b0bb88250d2595bd
Nikita Shah <niki.shah499@gmail.com>
2024-02-22 18:42:48 +0530

    added index outside src

---
+++ dist/cjs/src/index.d.ts
@@ -0,0 +1,7 @@
+import calculateTime from "./helpers/calculateTime";
+import healthCheck from "./helpers/healthCheck";
+declare const _default: {
+    calculateTime: typeof calculateTime;
+    healthCheck: typeof healthCheck;
+};
+export default _default;
