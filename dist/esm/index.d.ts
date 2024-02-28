89db9a1d68bde1cc87537bc108f2d41ae126b70f
Nikita Shah <niki.shah499@gmail.com>
2024-02-28 12:06:40 +0530

    test

--- dist/esm/index.d.ts
+++ dist/esm/index.d.ts
@@ -1,3 +1,2 @@
-export { calculateTime } from './src/helpers/calculateTime/calculateTime';
 export declare function healthCheck(): void;
 export declare function formatTime(time: string, format: string): string;
--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 import moment from "moment";
 // export * as calculateTime from './src/helpers/calculateTime/calculateTime';
 // export * as healthCheck from './src/helpers/healthCheck/healthCheck';
-export { calculateTime } from './src/helpers/calculateTime/calculateTime';
+// export { calculateTime } from './src/helpers/calculateTime/calculateTime';
 // export { healthCheck } from './src/helpers/healthCheck/healthCheck';
 
 
 export function healthCheck() {
-  console.log('healthCheck');
+  console.log('healthCheck qwqw');
 }
 
 export function formatTime(time: string, format: string) {
